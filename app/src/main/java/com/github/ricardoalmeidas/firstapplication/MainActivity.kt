8fa25ce8d627d978a94a86231252775e84286395
Ricardo <ricardo.almeidasantos04@gmail.com>
2026-03-13 12:19:20 -0300

    Remove exemplos e limpa a MainActivity para futura implementação

--- app/src/main/java/com/github/ricardoalmeidas/firstapplication/MainActivity.kt
+++ app/src/main/java/com/github/ricardoalmeidas/firstapplication/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstApplicationTheme {
-        Greeting("Android")
     }
 }
